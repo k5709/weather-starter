01b64a2d20a8c47288b259dd23effa04a96f217b
Kristian Rosales <r.kristian40@gmail.com>
2024-02-06 12:52:47 -0600

    finished rendering the cities to the header

---
+++ react-weather-app/src/components/inputs.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import{UilSearch, UilLocationPoint} from '@iconscout/react-unicons'
+
+export default function Inputs() {
+    return(
+        <div className='flex flex-row justify-center my-6'>
+            <div className='flex flex-row w-3/4 items-center justify-center spasce-x-4'>
+                input
+            </div>
+        </div>
+    )
+}
