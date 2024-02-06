8af0a15d70776e97e72610d2b87563535afff089
Kristian Rosales <r.kristian40@gmail.com>
2024-02-06 11:06:49 -0600

    project setup, ready to start implementing.

---
+++ react-weather-app/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import UilReact from '@iconscout/react-unicons';
+
+function App() {
+  return (
+    <div className="App">
+        <h1 className='text-2xl font-bold text-red-500'>
+          Hello World!
+        </h1>
+    </div>
+  );
+}
+
+export default App;
