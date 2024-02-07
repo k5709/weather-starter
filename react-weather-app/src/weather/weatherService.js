614e7abf7d6cc95a81421e73b4f5e87da23749fc
Kristian Rosales <r.kristian40@gmail.com>
2024-02-07 13:36:52 -0600

    getWeather API integrated, destructred and formatted data from API.

---
+++ react-weather-app/src/weather/weatherService.js
@@ -0,0 +1,49 @@
+const API_KEY = "b12d2f5b6dd2c1b6b40e57c2e8b99fe2";
+const BASE_URL = "https://api.openweathermap.org/data/2.5";
+
+const getWeatherData = (infoType, searchParams) => {
+  const url = new URL(BASE_URL + "/" + infoType);
+  url.search = new URLSearchParams({ ...searchParams, appId: API_KEY });
+
+  return fetch(url).then((res) => res.json());
+};
+
+const formatCurrentWeather = (data) => {
+  const {
+    coord: { lat, lon },
+    main: { temp, feels_like, temp_min, temp_max, humidity },
+    name,
+    dt,
+    sys: { country, sunrise, sunset },
+    weather,
+    wind: { speed },
+  } = data;
+
+  const { main: details, icon } = weather(0);
+
+  return {
+    lat,
+    lon,
+    temp,
+    feels_like,
+    temp_min,
+    temp_max,
+    humidity,
+    name,
+    dt,
+    country,
+    sunrise,
+    sunset,
+    weather,
+    speed,
+  };
+};
+
+const getFormattedWeatherData = async (searchParams) => {
+  const FormattedWeatherData = await getWeatherData(
+    "weather",
+    searchParams
+  ).then(formatCurrentWeather);
+};
+
+export default getWeatherData;
